e74a5c2cf7f5867dd74f3abe2131852506d9bc04
Matheus Fugisaki <matheus.maciel@objective.com.br>
2021-08-10 09:39:04 -0300

    feat: update code semantic

--- src/crawler.js
+++ src/crawler.js
@@ -6,7 +6,7 @@ const fetchPage = async link => {
   return data
 }
 
-const all = (items, fn) => {
+const waitAll = (items, fn) => {
   const promises = items.map(item => fn(item))
   return Promise.all(promises)
 }
@@ -23,28 +23,19 @@ const serialize = (items, fn) => {
     .then(() => result)
 }
 
-const splitToChunks = items => {
+const splitToChunks = (items, splitter = 40) => {
   const result = []
-  for (let i = 0; i < items.length; i += 50) {
-    result.push(items.slice(i, i + 50))
+  for (let i = 0; i < items.length; i += splitter) {
+    result.push(items.slice(i, i + splitter))
   }
   return result
 }
 
-const buildCharpter = (items, fn) => {
-  let result = []
-  const chunks = splitToChunks(items, 50)
-  return serialize(chunks, chunk =>
-    all(chunk, fn).then(res => result = result.concat(res))
-  )
-    .then(() => result)
-}
-
-const getCharpter = async ({ number, link }) => {
+const buildCharpter = async ({ number, link }) => {
   const chapterPage = await fetchPage(link)
   const c = cheerio.load(chapterPage)
   const pages = []
-  c('div.manga-pages > center > img').each((i, pag) => {
+  c('div.manga-pages > center > img').each((_, pag) => {
     pages.push(c(pag).attr('src'))
   })
 
@@ -52,6 +43,15 @@ const getCharpter = async ({ number, link }) => {
     number,
     pages,
   }
+}
+
+const getCharpter = (items, fn) => {
+  let result = []
+  const chunks = splitToChunks(items)
+  return serialize(chunks, chunk =>
+    waitAll(chunk, fn).then(res => result = result.concat(res))
+  )
+    .then(() => result)
 }
 
 const search = async link => {
@@ -63,14 +63,14 @@ const search = async link => {
   const synopsis = m('div.manga-synopsis').text()
 
   const items = []
-  m('div.manga-chapters > div.single-chapter').each((i, char) => {
+  m('div.manga-chapters > div.single-chapter').each((_, char) => {
     const chapter = m(char).find('a')
     const number = chapter.text().split('#')[1]
     const link = chapter.attr('href')
     items.push({ number, link })
   })
 
-  const charpters = await buildCharpter(items, getCharpter)
+  const charpters = await getCharpter(items, buildCharpter)
 
   return {
     title,
